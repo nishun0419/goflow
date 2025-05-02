46935625d29c4420e4a490190d29fa001d01cb65
SHUN <ninosekunlight@gmail.com>
2025-05-02 18:48:47 +0900

    migrationを走らせないようにする

--- backend/railway_start.sh
+++ backend/railway_start.sh
@@ -1,12 +1,5 @@
 #!/bin/sh
 set -e
 
-echo "Waiting for DB..."
-sleep 5  # ← 遅延させる（DB接続待ち）
-
-echo "Running database migrations..."
-GOOSE_DBSTRING="$DB_USER:$DB_PASSWORD@tcp($DB_HOST:$DB_PORT)/$DB_NAME" \
- go run github.com/pressly/goose/v3/cmd/goose@latest -dir db/migrations up
-
 echo "Starting app..."
 ./main
