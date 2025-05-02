c8d16483ac1e0d208d304127112c6f4bcd174e73
SHUN <ninosekunlight@gmail.com>
2025-05-02 18:06:06 +0900

    goose実行のコマンド変更

--- backend/railway_start.sh
+++ backend/railway_start.sh
@@ -6,7 +6,7 @@ sleep 5  # ← 遅延させる（DB接続待ち）
 
 echo "Running database migrations..."
 GOOSE_DBSTRING="$DB_USER:$DB_PASSWORD@tcp($DB_HOST:$DB_PORT)/$DB_NAME" \
-  goose -dir db/migrations mysql up
+ go run github.com/pressly/goose/v3/cmd/goose@latest -dir db/migrations up
 
 echo "Starting app..."
 ./main
